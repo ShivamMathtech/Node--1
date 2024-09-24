11c94976048d5ff8a633a85f0fddadbddab57ea9
ShivamMathtech <drshivam49@gmail.com>
2024-09-24 12:35:34 +0530

    va

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("Hello this is server.js");
+const express = require("express");
+const app = express();
+
+app.get("/hello", function (req, res) {
+  res.send("hi user");
+});
+app.get("/anil", function (req, res) {
+  res.send("hi anil");
+});
+app.get("/shivam", function (req, res) {
+  res.send("hi shivam");
+});
+app.get("/rajat", function (req, res) {
+  res.send("hi rajat");
+});
+
+app.listen(4000);
